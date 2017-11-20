8d26348e971fa1ffdc27e0c9fcffbbb9044f6e16
Robin <746129928@qq.com>
2017-11-20 17:06:13 +0800

    完成图文列表页

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import Home from '../components/home/home.vue';
 import NL from '../components/news/list.vue';
 import ND from '../components/news/detail.vue';
+import PhotoList from '../components/photo/list.vue';
 export default {
     routes:[
         {path:'/',redirect: '/index'},
         {path : '/index',component:Home},
         // 新闻路由
         {path : '/news/list',component:NL},
-        {path : '/news/detail/:id',component:ND}
+        {path : '/news/detail/:id',component:ND},
+        // 图片
+        {name:'pl', path : '/photo/list/:id',component:PhotoList},   
     ]
 }

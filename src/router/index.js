8ab426a1a859d370bab5c6cccd11e30d04db6ec3
Robin <746129928@qq.com>
2017-11-20 20:03:00 +0800

    完成图片详情页

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Home from '../components/home/home.vue';
 import NL from '../components/news/list.vue';
 import ND from '../components/news/detail.vue';
 import PhotoList from '../components/photo/list.vue';
+import PhotoDetail from '../components/photo/detail.vue';
 export default {
     routes:[
         {path:'/',redirect: '/index'},
@@ -10,6 +11,7 @@ export default {
         {path : '/news/list',component:NL},
         {path : '/news/detail/:id',component:ND},
         // 图片
-        {name:'pl', path : '/photo/list/:id',component:PhotoList},   
+        {name:'pl', path : '/photo/list/:id',component:PhotoList},  
+        {name:'pd', path : '/photo/detail/:id',component:PhotoDetail},  
     ]
 }
